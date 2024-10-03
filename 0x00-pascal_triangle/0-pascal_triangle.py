9babfb8373c97ea12e10a33d06077715bf82f3fd
Houssame <hossame@inbox.ru>
2024-10-03 21:46:50 +0100

    test4

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,13 +3,14 @@
 0-pascal_triangle 1 2 3
 """
 
+
 def pascal_triangle(n):
     """
     Generates Pascal's triangle of size n.
-    
+
     Args:
         n (int): The number of rows in the triangle.
-        
+
     Returns:
         list of lists: A list of lists representing Pascal's triangle.
     """
@@ -17,15 +18,15 @@ def pascal_triangle(n):
         return []
 
     triangle = []
-    
+
     for i in range(n):
         # Start with an empty row
         row = [1] * (i + 1)  # Create a row with '1's
-        
-        # Each element (except the first and last) is the sum of the two elements above
+
+        # Each element (except the first and last)
         for j in range(1, i):
             row[j] = triangle[i-1][j-1] + triangle[i-1][j]
-        
+
         triangle.append(row)
-    
+
     return triangle
