5b24bd62fe5bd5a134e2253a5dd25ff7ce4467c3
Houssame <hossame@inbox.ru>
2024-11-15 02:01:37 +0100

    0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,26 +1,23 @@
 #!/usr/bin/node
+// prints all characters of a Star Wars movie
 
 const request = require('request');
-const req = (arr, i) => {
-  if (i === arr.length) return;
-  request(arr[i], (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      console.log(JSON.parse(body).name);
-      req(arr, i + 1);
-    }
-  });
-};
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 
-request(
-  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
-  (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      const chars = JSON.parse(body).characters;
-      req(chars, 0);
-    }
+function charRequest (characters, num) {
+  if (num >= characters.length) return;
+  request.get(characters[num], (_req, resp) => {
+    const charBody = JSON.parse(resp.body);
+    const charName = charBody.name;
+    console.log(charName);
+    charRequest(characters, num + 1);
+  });
+}
+
+request.get(url, (_req, resp) => {
+  if (resp) {
+    const body = JSON.parse(resp.body);
+    const characters = body.characters;
+    charRequest(characters, 0);
   }
-);
+});
