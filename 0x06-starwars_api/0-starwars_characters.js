6400a7c4c3c1612e00fa4f0ca10a52f647c4a78e
Houssame <hossame@inbox.ru>
2024-11-15 01:52:04 +0100

    0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,26 +2,23 @@
 
 const request = require('request');
 
-const req = (arr, i) => {
-  if (i === arr.length) return;
-  request(arr[i], (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      console.log(JSON.parse(body).name);
-      req(arr, i + 1);
-    }
-  });
-};
-
-request(
-  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
-  (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      const chars = JSON.parse(body).characters;
-      req(chars, 0);
-    }
+request(`https://swapi.dev/api/films/${process.argv[2]}/`, (err, res, body) => {
+  if (err) {
+    console.log(err);
+    return;
   }
-);
+
+  const data = JSON.parse(body);
+
+  const characters = data.characters;
+  for (let i = 0; i < characters.length; i++) {
+    request(characters[i], (err, res, body) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      const character = JSON.parse(body);
+      console.log(character.name);
+    });
+  }
+});
