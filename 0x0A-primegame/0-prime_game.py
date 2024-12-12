ed159272af4ca7bfe441a49d97009e48e5519201
Elpira <elpira@elpira.168.1.36>
2024-12-12 21:06:35 +0100

    prime

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,38 +1,59 @@
 #!/usr/bin/python3
-"""initializing the module"""
+"""Prime Game - Maria and Ben are playing a game"""
 
 
-def isWinner(x, nums):
+def prime_game(rounds, nums):
+    """Determine the winner of multiple rounds of the prime game.
+
+    Args:
+        rounds (int): The number of rounds.
+        nums (list): List of integers for each round.
+
+    Returns:
+        str: The name of the player who won the most rounds ('Maria' or 'Ben').
+              If there is no winner, returns None.
     """
-    this function returns the name of the player that won the most rounds
-    """
-    def sieve_of_eratosthenes(limit):
-        primes = [True] * (limit + 1)
-        primes[0], primes[1] = False, False
-        for i in range(2, int(limit ** 0.5) + 1):
-            if primes[i]:
-                for j in range(i * i, limit + 1, i):
-                    primes[j] = False
-        return primes
+    if rounds <= 0 or nums is None:
+        return None
+    if rounds != len(nums):
+        return None
 
-    def game_round(n):
-        primes = sieve_of_eratosthenes(n)
-        count = 0
-        for i in range(2, n + 1):
-            if primes[i]:
-                count += 1
-        return count % 2 == 1
+    player_ben = 0
+    player_maria = 0
 
-    maria_wins = 0
-    ben_wins = 0
+    # Create a list to mark prime numbers using Sieve of Eratosthenes
+    max_number = sorted(nums)[-1]
+    sieve = [1 for _ in range(max_number + 1)]
+    sieve[0], sieve[1] = 0, 0  # 0 and 1 are not prime
+
+    # Mark multiples of primes as 0
+    for i in range(2, len(sieve)):
+        if sieve[i] == 1:  # i is prime
+            remove_multiples(sieve, i)
+
+    # Count the number of wins for each player based on the rounds
     for n in nums:
-        if game_round(n):
-            maria_wins += 1
+        prime_count = sum(sieve[:n + 1])
+        if prime_count % 2 == 0:
+            player_ben += 1
         else:
-            ben_wins += 1
+            player_maria += 1
 
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
+    # Determine the overall winner
+    if player_ben > player_maria:
         return "Ben"
+    if player_maria > player_ben:
+        return "Maria"
     return None
+
+
+def remove_multiples(sieve_list, prime):
+    """Removes multiples of a prime number from the list.
+
+    Args:
+        sieve_list (list): List of integers where prime multiples.
+        prime (int): The prime number whose multiples will be removed.
+    """
+    for i in range(2, len(sieve_list)):
+        if i * prime < len(sieve_list):
+            sieve_list[i * prime] = 0
